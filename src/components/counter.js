e29c9664111acc386dc1e24cf6580addda6bf03d
Yura <merda@tut.by>
2022-02-09 05:18:34 +0300

    conect (mapDispatchTo props + mapStateToProps)

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,4 +1,5 @@
 import { connect } from "react-redux";
+import { inc , dec, rnd} from "../action";
 
 const Counter = ({counter, inc, dec, rnd}) =>{
 
@@ -12,4 +13,23 @@ const Counter = ({counter, inc, dec, rnd}) =>{
     )
 }
 
-export default Counter;
+const mapStateToProps = (state) => {
+    return {
+        counter: state.value,
+        smt: state.foo
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        inc: () => dispatch(inc()),
+        dec: () => dispatch(dec()),
+        rnd: () => {
+            const value = Math.floor(Math.random() * 10);
+            dispatch(rnd(value));
+        }
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
